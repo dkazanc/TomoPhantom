553a7498eb0af3e3168cece5e224cacd94799e4d
Daniil Kazantsev <dkazanc@hotmail.com>
2017-07-02 12:39:30 +0100

    TomoPhantom0.1

---
+++ matlab/PhantomGeneratorDemo.m
@@ -0,0 +1,34 @@
+% Script to generate analytical phantoms that can be used to test
+% reconstruction algorithms.
+% If one needs to modify/add phantoms just edit PhantomLibrary.dat
+% ver 0.1, 1.07.17
+
+close all;clc;clear all;
+% adding paths
+addpath('../models/'); addpath('supp/'); 
+
+ModelNo = 41; % Select a model (0 - 42)
+% Define phantom dimension
+N = 512; % x-y size (squared image)
+
+% generate the 2D phantom:
+[G] = buildPhantom(ModelNo,N);
+figure; imshow(G, []);
+
+%%
+% perform deformation according to the tranform proposed in paper [1] (see readme)
+FocalP = 4; % focal point distance
+RFP = 1./FocalP;
+DeformType = 0; % 0 - forward, 1 - inverse 
+angle = 15;
+angles_rad = (pi/180)*angle;
+
+% deform forward
+G_deformed = DeformObject(G, RFP, angles_rad, DeformType);
+% deform back
+DeformType = 1;
+G_inv = DeformObject(G_deformed, RFP, angles_rad, DeformType);
+figure; 
+subplot(1,2,1); imshow(G_deformed, []); title('Deformed Phantom');
+subplot(1,2,2); imshow(G_inv, []); title('Inversely Deformed Phantom');
+%%
---
+++ matlab/supp/Deform_Inverse_IMG_F.m
@@ -0,0 +1,34 @@
+function [Image_G] = Deform_Inverse_IMG_F(G, angle,...
+    SizeimageX, SizeimageY, Tomorange_X_Ar, Tomorange_Y_Ar, FocalP)
+
+%Inverse deformation of an object from a given angle
+
+angles_rad = (pi/180)*angle;
+
+SizeX = size(G,1);
+SizeY = size(G,2);
+
+Image_G = zeros(SizeX, SizeY);
+            
+for i = 1:SizeimageX          
+    for j = 1:SizeimageY
+           
+     
+           xx = Tomorange_X_Ar(i)*cos(angles_rad) + Tomorange_Y_Ar(j)*sin(angles_rad);
+           yy = -Tomorange_X_Ar(i)*sin(angles_rad) + Tomorange_Y_Ar(j)*cos(angles_rad);
+          
+           xPersp1 = xx/(1 - yy/FocalP);
+           yPersp1 = yy;
+           xPersp = xPersp1/(1 - yPersp1/FocalP)*cos(angles_rad) - yPersp1*sin(angles_rad);
+           yPersp = xPersp1/(1 - yPersp1/FocalP)*sin(angles_rad) + yPersp1*cos(angles_rad);
+        
+%            xPersp = Tomorange_X_Ar(i)*(1 - Tomorange_Y_Ar(j)/FocalP)*cos(angles_rad) - Tomorange_Y_Ar(j)*sin(angles_rad);
+%            yPersp = Tomorange_X_Ar(i)*(1 - Tomorange_Y_Ar(j)/FocalP)*sin(angles_rad) + Tomorange_Y_Ar(j)*cos(angles_rad);
+                        
+            Image_G(j,i) = BilinearInterpolation(G, Tomorange_Y_Ar, Tomorange_X_Ar, yPersp, xPersp);     
+                        
+    end %for
+end %for
+            
+
+return
