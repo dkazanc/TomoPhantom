a4363c84942517a347af0113bbfabc709e40ae0b
Srikanth Nagella <srikanth.nagella@stfc.ac.uk>
2017-10-13 17:13:59 +0100

    Modified setup to package model files. Added test case.

--- python/tests/test_phantom3d.py
+++ python/tests/test_phantom3d.py
@@ -1,8 +1,18 @@
 import unittest
 import numpy as np
+import tomophantom
 import tomophantom.phantom3d
-
+import os
 class TestTomophantom3D(unittest.TestCase):
     def test_create_phantom3d(self):
-        data = phantom3d.build_phantom_3d('models/Phantom3DLibrary.dat',1,256)
-        self.assetEqual(data.shape, (256,256))
+        [tpath, filename] = os.path.split(os.path.abspath(tomophantom.__file__))
+        libpath = os.path.join(tpath,'models/Phantom3DLibrary.dat')
+        print(libpath)
+        data = tomophantom.phantom3d.build_phantom_3d(libpath,1,256)
+        self.assertEqual(data.shape, (256,256,256))
+        self.assertNotEqual(data[128,128,128], 0.0)
+        self.assertEqual(data[0,0,0],0.0)
+        
+        
+if __name__ == "__main__":
+    unittest.main()
