de858d1ffc628f1a440a39079af19d01ceba7be2
Daniil Kazantsev <dkazanc@hotmail.com>
2018-02-07 23:04:45 +0000

    python demo updated and readme

--- python/Demos/DemoTomoPhantom.py
+++ python/Demos/DemoTomoPhantom.py
@@ -15,7 +15,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from tomophantom import phantom2d
 from tomophantom import phantom3d
-from astraOP import AstraOper
+from astraOP import AstraTools
 #%%
 model = 11
 N_size = 512
@@ -28,22 +28,21 @@ plt.figure(1)
 plt.imshow(phantom_2D, cmap="hot")
 plt.title('2D Phantom')
 #%%
+# create sinogram analytically
 angles_num = int(0.5*np.pi*N_size); # angles number
 angles = np.linspace(0,180,angles_num,dtype='float32')
 angles_rad = angles*(np.pi/180)
 P = int(np.sqrt(2)*N_size) #detectors
 
-# create sinogram analytically
 sino_an = phantom2d.buildSino2D(model, N_size, P, angles, pathTP, 1)
 
 plt.figure(2)
 plt.imshow(sino_an, cmap="hot")
 plt.title('Analytical sinogram')
 #%%
-# create sinogram using ASTRA toolbox
-A = AstraOper(P, angles_rad - 0.5*np.pi, N_size) # initiate a class object
-sino_num_ASTRA = A.forwproj(phantom_2D) # generate numerical sino (Ax)
-#x = A.backproj(sino_an) # generate backprojection (A'b)
+Atools = AstraTools(P, angles_rad - 0.5*np.pi, N_size, 'cpu') # initiate a class object
+sino_num_ASTRA = Atools.forwproj(phantom_2D) # generate numerical sino (Ax)
+#x = Atools.backproj(sino_an) # generate backprojection (A'b)
 
 plt.figure(2) 
 plt.subplot(121)
@@ -55,11 +54,18 @@ plt.title('Numerical sinogram')
 plt.show()
 #%%
 print ("Reconstructing using FBP...")
-FBP = A.fbp2D(sino_an)
+FBPrec = Atools.fbp2D(sino_an)
 
 plt.figure(3) 
-plt.imshow(FBP, vmin=0, vmax=1, cmap="hot")
-plt.title('Reconstructed Phantom')
+plt.imshow(FBPrec, vmin=0, vmax=1, cmap="hot")
+plt.title('FBP Reconstructed Phantom')
+#%%
+print ("Reconstructing using SIRT...")
+SIRTrec = Atools.sirt2D(sino_an, 10)
+
+plt.figure(4) 
+plt.imshow(SIRTrec, vmin=0, vmax=1, cmap="hot")
+plt.title('SIRT Reconstructed Phantom')
 
 #%%
 print ("Building 3D phantom using TomoPhantom software")
@@ -71,7 +77,7 @@ pathTP3 = '/home/algol/Documents/DEV/TomoPhantom/functions/models/Phantom3DLibra
 phantom_tm = phantom3d.buildPhantom3D(model, N_size, pathTP3)
 
 #plt.gray()
-plt.figure(4) 
+plt.figure(5) 
 plt.subplot(121)
 plt.imshow(phantom_tm[128,:,:],vmin=0, vmax=1, cmap="hot")
 plt.title('3D Phantom, axial view')
