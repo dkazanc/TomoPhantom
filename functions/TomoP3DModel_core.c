b74e0ee0b112ea6884cc62631c5c8c3b4b513f44
Daniil Kazantsev <dkazanc@hotmail.com>
2018-08-13 09:03:14 +0100

    matrix respresentation corrected in utils

--- functions/TomoP3DModel_core.c
+++ functions/TomoP3DModel_core.c
@@ -58,144 +58,157 @@ float TomoP3DObject_core(float *A, int N, char *Object,
         int tt /*temporal index, 0 - for stationary */)
 {
     int i, j, k;
-    float Tomorange_Xmin, Tomorange_Xmax, H_x, C1, a2, b2, c2, phi_rot_radian, sin_phi, cos_phi, aa,bb,cc, psi1, psi2, psi3, T;
-    float *Tomorange_X_Ar=NULL, *Xdel = NULL, *Ydel = NULL, *Zdel = NULL;
-    Tomorange_X_Ar = malloc(N*sizeof(float));
-    if(Tomorange_X_Ar == NULL ) printf("Allocation of 'Tomorange_X_Ar' failed");
-    Tomorange_Xmin = -1.0f;
-    Tomorange_Xmax = 1.0f;
-    H_x = (Tomorange_Xmax - Tomorange_Xmin)/(N);
-    for(i=0; i<N; i++)  {Tomorange_X_Ar[i] = Tomorange_Xmin + (float)i*H_x;}
-    C1 = -4.0f*logf(2.0f);
+//     float Tomorange_Xmin, Tomorange_Xmax, H_x, C1, a2, b2, c2, phi_rot_radian, sin_phi, cos_phi, aa,bb,cc, psi1, psi2, psi3, T;
+//     float *Tomorange_X_Ar=NULL, *Xdel = NULL, *Ydel = NULL, *Zdel = NULL;
+//     Tomorange_X_Ar = malloc(N*sizeof(float));
+//     if(Tomorange_X_Ar == NULL ) printf("Allocation of 'Tomorange_X_Ar' failed");
+//     Tomorange_Xmin = -1.0f;
+//     Tomorange_Xmax = 1.0f;
+//     H_x = (Tomorange_Xmax - Tomorange_Xmin)/(N);
+//     for(i=0; i<N; i++)  {Tomorange_X_Ar[i] = Tomorange_Xmin + (float)i*H_x;}
+//     C1 = -4.0f*logf(2.0f);
+//     
+//     /* parameters of a model have been extracted, now run the building module */
+//     /************************************************/
+//     phi_rot_radian = psi_gr1*((float)M_PI/180.0f);
+//     sin_phi=sinf(phi_rot_radian); cos_phi=cosf(phi_rot_radian);
+//     
+//     Xdel = malloc(N*sizeof(float));
+//     if(Xdel == NULL ) printf("Allocation of 'Xdel' failed");
+//     Ydel = malloc(N*sizeof(float));
+//     if(Ydel == NULL ) printf("Allocation of 'Ydel' failed");
+//     Zdel = malloc(N*sizeof(float));
+//     if(Zdel == NULL ) printf("Allocation of 'Zdel' failed");
+//     for(i=0; i<N; i++)  {
+//         Xdel[i] = Tomorange_X_Ar[i] - x0;
+//         Ydel[i] = Tomorange_X_Ar[i] - y0;
+//         Zdel[i] = Tomorange_X_Ar[i] - z0;
+//     }
+//     
+//     psi1 = psi_gr1*((float)M_PI/180.0f);
+//     psi2 = psi_gr2*((float)M_PI/180.0f);
+//     psi3 = psi_gr3*((float)M_PI/180.0f);
+//     
+//     //float bs[9];
+//     float bs[3][3] = {
+//     {0.0f,0.0f,0.0f},
+//     {0.0f,0.0f,0.0f},
+//     {0.0f,0.0f,0.0f}
+//     };
+//   
+//     float xh[3] = {0.0f, 0.0f, 0.0f};
+//     float xh3[3] = {0.0f, 0.0f, 0.0f};
+//     
+//     a2 = 1.0f/(a*a);
+//     b2 = 1.0f/(b*b);
+//     c2 = 1.0f/(c*c);
+//     
+//     matrot3(bs,psi1,psi2,psi3); /* rotation of 3x3 matrix */
+//     
+//     xh3[0] = x0; xh3[1] = y0; xh3[2] = z0;
+//     matvet3(bs,xh3,xh);  /* matrix-vector multiplication */
+//     
+//     float xh1[3] = {0.0f, 0.0f, 0.0f};
+//     float xh2[3] = {0.0f, 0.0f, 0.0f};
     
-    /* parameters of a model have been extracted, now run the building module */
-    /************************************************/
-    phi_rot_radian = psi_gr1*((float)M_PI/180.0f);
-    sin_phi=sinf(phi_rot_radian); cos_phi=cosf(phi_rot_radian);
-    
-    Xdel = malloc(N*sizeof(float));
-    if(Xdel == NULL ) printf("Allocation of 'Xdel' failed");
-    Ydel = malloc(N*sizeof(float));
-    if(Ydel == NULL ) printf("Allocation of 'Ydel' failed");
-    Zdel = malloc(N*sizeof(float));
-    if(Zdel == NULL ) printf("Allocation of 'Zdel' failed");
-    for(i=0; i<N; i++)  {
-        Xdel[i] = Tomorange_X_Ar[i] - x0;
-        Ydel[i] = Tomorange_X_Ar[i] - y0;
-        Zdel[i] = Tomorange_X_Ar[i] - z0;
-    }
-    
-    psi1 = psi_gr1*((float)M_PI/180.0f);
-    psi2 = psi_gr2*((float)M_PI/180.0f);
-    psi3 = psi_gr3*((float)M_PI/180.0f);
-    
-    float bs[9];
-    float xh[3];
-    float xh3[3];
-    
-    a2 = 1.0f/(a*a);
-    b2 = 1.0f/(b*b);
-    c2 = 1.0f/(c*c);
-    matrot3(bs,psi1,psi2,psi3); /* rotation of 3x3 matrix */
-    
-    xh3[0] = x0; xh3[1] = y0; xh3[2] = z0;
-    matvet3(bs,xh3,xh);  /* matrix-vector multiplication */
-    
-    float xh1[3];
-    float xh2[3];
-    
-    if ((strcmp("gaussian",Object) == 0) ||  (strcmp("paraboloid",Object) == 0) || (strcmp("ellipsoid",Object) == 0) || (strcmp("cone",Object) == 0)) {
- #pragma omp parallel for shared(A,bs) private(k,i,j,aa,bb,cc,T,xh2,xh1)
-        for(k=0; k<N; k++) {
-            for(i=0; i<N; i++) {
-                for(j=0; j<N; j++) {
-                    
-                    if ((psi1 != 0.0f) || (psi2 != 0.0f) || (psi3 != 0.0f)) {
-                        xh1[0]=Tomorange_X_Ar[i];
-                        xh1[1]=Tomorange_X_Ar[j];
-                        xh1[2]=Tomorange_X_Ar[k];
-                        matvet3(bs,xh1,xh2);
-                        aa = a2*powf((xh2[0]-xh[0]),2);
-                        bb = b2*powf((xh2[1]-xh[1]),2);
-                        cc = c2*powf((xh2[2]-xh[2]),2);
-                    }
-                    else {
-                        aa = a2*powf(Xdel[i],2);
-                        bb = b2*powf(Ydel[j],2);
-                        cc = c2*powf(Zdel[k],2);
-                    }
-                    T = (aa + bb + cc);
-                    if (strcmp("gaussian",Object) == 0) {
-                        /* The object is a volumetric gaussian */
-                        T = C0*expf(C1*T);
-                    }
-                    if (strcmp("paraboloid",Object) == 0) {
-                        /* the object is a parabola Lambda = 1/2 */
-                        if (T <= 1.0f) T = C0*sqrtf(1.0f - T);
-                        else T = 0.0f;
-                    }
-                    if (strcmp("ellipsoid",Object) == 0) {
-                        /* the object is en ellipsoid */
-                        if (T <= 1.0f) T = C0;
-                        else T = 0.0f;
-                    }
-                    if (strcmp("cone",Object) == 0) {
-                        /* the object is a cone */
-                        if (T <= 1.0f) T = C0*(1.0f - sqrtf(T));
-                        else T = 0.0f;
-                    }
-                    A[tt*N*N*N + (k)*N*N + j*N+i] += T;
-                }}}
-    }
-    if (strcmp("cuboid",Object) == 0) {
-        /* the object is a cuboid */
-        float x0r, y0r, HX, HY;
-        a2 = 0.5f*a;
-        b2 = 0.5f*b;
-        c2 = 0.5f*c;
-        x0r=x0*cosf(0.0f) + y0*sinf(0.0f);
-        y0r=-x0*sinf(0.0f) + y0*cosf(0.0f);
-        if (phi_rot_radian < 0.0f) {
-            phi_rot_radian = (float)M_PI + phi_rot_radian;
-            sin_phi=sinf(phi_rot_radian);
-            cos_phi=cosf(phi_rot_radian);
-        }
-#pragma omp parallel for shared(A,Zdel) private(k,i,j,HX,HY,T)
-        for(k=0; k<N; k++) {
-            if  (fabs(Zdel[k]) < c2) {
-                
-                for(i=0; i<N; i++) {
-                    for(j=0; j<N; j++) {
-                        HX = fabsf((Xdel[i] - x0r)*cos_phi + (Ydel[j] - y0r)*sin_phi);
-                        T = 0.0f;
-                        if (HX <= a2) {
-                            HY = fabsf((Ydel[j] - y0r)*cos_phi - (Xdel[i] - x0r)*sin_phi);
-                            if (HY <= b2) {T = C0;}
-                        }
-                        A[tt*N*N*N + (k)*N*N + j*N+i] += T;
-                    }
-                }
-            }
-        }
-    }
-    if (strcmp("elliptical_cylinder",Object) == 0) {
-        /* the object is an elliptical cylinder  */
-#pragma omp parallel for shared(A) private(k,i,j,T)
-        for(k=0; k<N; k++) {
-            if  (fabs(Zdel[k]) < c) {
-                for(i=0; i<N; i++) {
-                    for(j=0; j<N; j++) {
-                        T = a2*powf((Xdel[i]*cos_phi + Ydel[j]*sin_phi),2) + b2*powf((-Xdel[i]*sin_phi + Ydel[j]*cos_phi),2);
-                        if (T <= 1) T = C0;
-                        else T = 0.0f;
-                        A[tt*N*N*N + (k)*N*N + j*N+i] += T;
-                    }}
-            }
-        } /*k-loop*/
-    }
+//     if ((strcmp("gaussian",Object) == 0) ||  (strcmp("paraboloid",Object) == 0) || (strcmp("ellipsoid",Object) == 0) || (strcmp("cone",Object) == 0)) {
+//  #pragma omp parallel for shared(A,bs) private(k,i,j,aa,bb,cc,T,xh2,xh1)
+//         for(k=0; k<N; k++) {
+//             for(i=0; i<N; i++) {
+//                 for(j=0; j<N; j++) {
+//                     
+//                     if ((psi1 != 0.0f) || (psi2 != 0.0f) || (psi3 != 0.0f)) {
+//                         xh1[0]=Tomorange_X_Ar[i];
+//                         xh1[1]=Tomorange_X_Ar[j];
+//                         xh1[2]=Tomorange_X_Ar[k];
+//                         matvet3(bs,xh1,xh2);
+//                         aa = a2*powf((xh2[0]-xh[0]),2);
+//                         bb = b2*powf((xh2[1]-xh[1]),2);
+//                         cc = c2*powf((xh2[2]-xh[2]),2);
+//                     }
+//                     else {
+//                         aa = a2*powf(Xdel[i],2);
+//                         bb = b2*powf(Ydel[j],2);
+//                         cc = ctt*N*N*N +2*powf(Zdel[k],2);
+//                     }
+//                     T = (aa + bb + cc);
+//                     if (strcmp("gaussian",Object) == 0) {
+//                         /* The object is a volumetric gaussian */
+//                         T = C0*expf(C1*T);
+//                     }
+//                     if (strcmp("paraboloid",Object) == 0) {
+//                         /* the object is a parabola Lambda = 1/2 */
+//                         if (T <= 1.0f) T = C0*sqrtf(1.0f - T);
+//                         else T = 0.0f;
+//                     }
+//                     if (strcmp("ellipsoid",Object) == 0) {
+//                         /* the object is en ellipsoid */
+//                         if (T <= 1.0f) T = C0;
+//                         else T = 0.0f;
+//                     }
+//                     if (strcmp("cone",Object) == 0) {
+//                         /* the object is a cone */
+//                         if (T <= 1.0f) T = C0*(1.0f - sqrtf(T));
+//                         else T = 0.0f;
+//                     }
+//                     A[tt*N*N*N + (k)*N*N + j*N+i] += T;
+//                 }}}
+//     }
+//     if (strcmp("cuboid",Object) == 0) {
+//         /* the object is a cuboid */
+//         float x0r, y0r, HX, HY;
+//         a2 = 0.5f*a;
+//         b2 = 0.5f*b;
+//         c2 = 0.5f*c;
+//         x0r=x0*cosf(0.0f) + y0*sinf(0.0f);
+//         y0r=-x0*sinf(0.0f) + y0*cosf(0.0f);
+//         if (phi_rot_radian < 0.0f) {
+//             phi_rot_radian = (float)M_PI + phi_rot_radian;
+//             sin_phi=sinf(phi_rot_radian);
+//             cos_phi=cosf(phi_rot_radian);
+//         }
+// #pragma omp parallel for shared(A,Zdel) private(k,i,j,HX,HY,T)
+//         for(k=0; k<N; k++) {
+//             if  (fabs(Zdel[k]) < c2) {
+//                 
+//                 for(i=0; i<N; i++) {
+//                     for(j=0; j<N; j++) {
+//                         HX = fabsf((Xdel[i] - x0r)*cos_phi + (Ydel[j] - y0r)*sin_phi);
+//                         T = 0.0f;
+//                         if (HX <= a2) {
+//                             HY = fabsf((Ydel[j] - y0r)*cos_phi - (Xdel[i] - x0r)*sin_phi);
+//                             if (HY <= b2) {T = C0;}
+//                         }
+//                         A[tt*N*N*N + (k)*N*N + j*N+i] += T;
+//                     }
+//                 }
+//             }
+//         }
+//     }
+//     if (strcmp("elliptical_cylinder",Object) == 0) {
+//         /* the object is an elliptical cylinder  */
+// #pragma omp parallel for shared(A) private(k,i,j,T)
+//         for(k=0; k<N; k++) {
+//             if  (fabs(Zdel[k]) < c) {
+//                 for(i=0; i<N; i++) {
+//                     for(j=0; j<N; j++) {
+//                         T = a2*powf((Xdel[i]*cos_phi + Ydel[j]*sin_phi),2) + b2*powf((-Xdel[i]*sin_phi + Ydel[j]*cos_phi),2);
+//                         if (T <= 1) T = C0;
+//                         else T = 0.0f;
+//                         A[tt*N*N*N + (k)*N*N + j*N+i] += T;
+//                     }}
+//             }
+//         } /*k-loop*/
+//     }
+    #pragma omp parallel for shared(A) private(k,i,j)
+    for(k=0; k<N; k++) {
+          for(i=0; i<N; i++) {
+                  for(j=0; j<N; j++) {
+    A[(k)*N*N + j*N+i] = 10.0f;
+                  }}}
     /****************************************************/
-    free(Xdel); free(Ydel); free(Zdel);
-    free(Tomorange_X_Ar);
+//     free(Xdel); free(Ydel); free(Zdel);
+//     free(Tomorange_X_Ar);
     return *A;
 }
 
--- functions/utils.c
+++ functions/utils.c
@@ -504,35 +504,61 @@ float checkParams3D(int *params_switch, int ModelSelected, char *ModelParameters
 }
 
 /***********************************************************************************************/
-/* rotation matrix routine */
-float matrot3(float *Ad, float psi1, float psi2, float psi3)
+/* rotation matrix */
+// float matrot3(float *Ad, float psi1, float psi2, float psi3)
+// {
+//     Ad[0] = cosf(psi1)*cosf(psi2)*cosf(psi3)-sinf(psi1)*sinf(psi3);
+//     Ad[1] = sinf(psi1)*cosf(psi2)*cosf(psi3)+cosf(psi1)*sinf(psi3);
+//     Ad[2] = -sinf(psi2)*cosf(psi3);
+//     Ad[3] = -cosf(psi1)*cosf(psi2)*sinf(psi3)-sinf(psi1)*cosf(psi3);
+//     Ad[4] = -sinf(psi1)*cosf(psi2)*sinf(psi3)+cosf(psi1)*cosf(psi3);
+//     Ad[5] = sinf(psi2)*sinf(psi3);
+//     Ad[6] = cosf(psi1)*sinf(psi2);
+//     Ad[7] = sinf(psi1)*sinf(psi2);
+//     Ad[8] = cosf(psi2);
+//     return *Ad;
+// }
+float matrot3(float Ad[3][3], float psi1, float psi2, float psi3)
 {
-    Ad[0] = cosf(psi1)*cosf(psi2)*cosf(psi3)-sinf(psi1)*sinf(psi3);
-    Ad[1] = sinf(psi1)*cosf(psi2)*cosf(psi3)+cosf(psi1)*sinf(psi3);
-    Ad[2] = -sinf(psi2)*cosf(psi3);
-    Ad[3] = -cosf(psi1)*cosf(psi2)*sinf(psi3)-sinf(psi1)*cosf(psi3);
-    Ad[4] = -sinf(psi1)*cosf(psi2)*sinf(psi3)+cosf(psi1)*cosf(psi3);
-    Ad[5] = sinf(psi2)*sinf(psi3);
-    Ad[6] = cosf(psi1)*sinf(psi2);
-    Ad[7] = sinf(psi1)*sinf(psi2);
-    Ad[8] = cosf(psi2);
-    return *Ad;
+    Ad[0][0]=cosf(psi1)*cosf(psi2)*cosf(psi3)-sinf(psi1)*sinf(psi3);
+    Ad[0][1]=sinf(psi1)*cosf(psi2)*cosf(psi3)+cosf(psi1)*sinf(psi3);
+    Ad[0][2]=-sinf(psi2)*cosf(psi3);
+    Ad[1][0]=-cosf(psi1)*cosf(psi2)*sinf(psi3)-sinf(psi1)*cosf(psi3);
+    Ad[1][1]=-sinf(psi1)*cosf(psi2)*sinf(psi3)+cosf(psi1)*cosf(psi3);
+    Ad[1][2]=sinf(psi2)*sinf(psi3);
+    Ad[2][0]=cosf(psi1)*sinf(psi2);
+    Ad[2][1]=sinf(psi1)*sinf(psi2);
+    Ad[2][2]=cosf(psi2);
+    //return *Ad;
+    return 1;
 }
 
 /*matrix-vector multiplication*/
-float matvet3(float *Ad, float *V1, float *V2)
+// float matvet3(float *Ad, float *V1, float *V2)
+// {
+//     int l, m, counterT;
+//     
+//     counterT = 0;
+//     for(l=0; l<3; l++) {
+//         V2[l] = 0.0f;
+//         for(m=0; m<3; m++) {
+//             V2[l] += Ad[counterT]*V1[m];
+//             counterT++;
+//         }}
+//     return *V2;
+// }
+
+float matvet3(float Ad[3][3], float V1[3], float V2[3])
 {
-    int l, m, counterT;
-    
-    counterT = 0;
+    int l, m;        
     for(l=0; l<3; l++) {
         V2[l] = 0.0f;
         for(m=0; m<3; m++) {
-            V2[l] += Ad[counterT]*V1[m];
-            counterT++;
-        }}
-    return *V2;
+            V2[l] += Ad[l][m]*V1[m];            
+        }}    
+    return 1;
 }
+
 
 /*matrix-matrix multiplication*/
 // float matmat3(float *A, float *B, float *C)
