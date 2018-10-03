8bc14f5dd92f89c5cc345c46d1cc161e06729040
Edoardo Pasca <edo.paskino@gmail.com>
2018-10-03 15:54:23 +0100

    working conda recipe for windows

--- Wrappers/Python/conda-recipe/bld.bat
+++ Wrappers/Python/conda-recipe/bld.bat
@@ -1,8 +1,11 @@
 mkdir "%SRC_DIR%\tomophantom"
-xcopy /e "%RECIPE_DIR%\..\.." "%SRC_DIR%\tomophantom"
-cd tomophantom\python
+ROBOCOPY /E "%RECIPE_DIR%\.." "%SRC_DIR%\tomophantom"
+cd tomophantom
 
-%PYTHON% setup.py build_ext
+:: issue cmake to create setup.py
+cmake -G "NMake Makefiles" %RECIPE_DIR%\..\..\..\ -DBUILD_PYTHON_WRAPPERS=ON -DCONDA_BUILD=ON -DLIBRARY_LIB="%CONDA_PREFIX%\lib" -DLIBRARY_INC="%CONDA_PREFIX%" -DCMAKE_INSTALL_PREFIX="%PREFIX%\Library"
+nmake install
+::%PYTHON% setup.py build_ext
 if errorlevel 1 exit 1
-%PYTHON% setup.py install
-if errorlevel 1 exit 1
+::%PYTHON% setup.py install
+::if errorlevel 1 exit 1
