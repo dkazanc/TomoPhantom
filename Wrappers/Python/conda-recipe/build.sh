94ba6bd611e17a2c832b5ee6613612f11edb31f1
Edoardo Pasca <edo.paskino@gmail.com>
2018-10-04 16:58:57 +0100

    fixes conda build, adds library

--- Wrappers/Python/conda-recipe/build.sh
+++ Wrappers/Python/conda-recipe/build.sh
@@ -1,9 +1,10 @@
 
 
 mkdir ${SRC_DIR}/tomophantom
-cp -r "${RECIPE_DIR}/.." ${SRC_DIR}/tomophantom
+#cp -r "${RECIPE_DIR}/.." ${SRC_DIR}/tomophantom
 cd tomophantom
 
 #issue cmake to create setup.py
-cmake -G "Unix Makefiles" ${RECIPE_DIR}/../../../ -DBUILD_PYTHON_WRAPPERS=ON -DCONDA_BUILD=ON -DCMAKE_BUILD_TYPE="Release" -DLIBRARY_LIB=$CONDA_PREFIX/lib -DLIBRARY_INC=$CONDA_PREFIX -DCMAKE_INSTALL_PREFIX=$PREFIX
+cmake -G "Unix Makefiles" ${RECIPE_DIR}/../../../ -DBUILD_PYTHON_WRAPPER=ON -DCONDA_BUILD=ON -DCMAKE_BUILD_TYPE="Release" -DLIBRARY_LIB=$CONDA_PREFIX/lib -DLIBRARY_INC=$CONDA_PREFIX -DCMAKE_INSTALL_PREFIX=$PREFIX
+#cp -rv "${RECIPE_DIR}/../../../PhantomLibrary" ${SRC_DIR}/tomophantom/Wrappers/Python/tomophantom
 make install
