727ca8bb7fbff5d8697899a1ff6b9c33a6c64d0a
Edoardo Pasca <14138589+paskino@users.noreply.github.com>
2026-02-11 11:09:02 +0000

    update build scripts

--- .scripts/conda_upload.sh
+++ .scripts/conda_upload.sh
@@ -9,7 +9,7 @@ mkdir ~/conda-bld
 conda config --set anaconda_upload no
 export CONDA_BLD_PATH=~/conda-bld
 
-export CIL_VERSION=3.0.1
+export CIL_VERSION=3.0.2
 $CONDA/bin/conda build conda-recipe . -c httomo
 
 # upload packages to conda
