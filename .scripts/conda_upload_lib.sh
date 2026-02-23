78a25dd1c8349b0b127b580c82c85d572fe9a9fb
Edoardo Pasca <14138589+paskino@users.noreply.github.com>
2026-02-23 15:21:03 +0000

    setup conda and use specific python version from matrix in CI run GHA on PR without upload

--- .scripts/conda_upload_lib.sh
+++ .scripts/conda_upload_lib.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+is_pr=0
+while getopts :p option; do
+  case "${option}" in
+  p) is_pr=1 ;; # pull request
+  *) ;;
+  esac
+done
 
 PKG_NAME=libtomophantom
 USER=httomo-team
@@ -8,12 +15,18 @@ mkdir ~/conda-bld
 conda config --set anaconda_upload no
 export CONDA_BLD_PATH=~/conda-bld
 
-export CIL_VERSION=3.0.2
+export CIL_VERSION=3.0.3
 $CONDA/bin/conda build conda-recipe_library . -c httomo
 
-# upload packages to conda
-find $CONDA_BLD_PATH/$OS -name *.conda | while read file
-do
-    echo $file
-    $CONDA/bin/anaconda -v --show-traceback -t $ANACONDA_API_TOKEN upload $file --force
-done
+if [ $is_pr -eq 1 ]; then
+    echo "Pull request detected, skipping conda upload."
+    exit 0
+else
+    # upload packages to conda
+    find $CONDA_BLD_PATH/$OS -name *.conda | while read file
+    do
+        echo $file
+        $CONDA/bin/anaconda -v --show-traceback -t $ANACONDA_API_TOKEN upload $file --force
+    done
+
+fi
